c84dd27265e5d171cce7431812bab6653dcd61a8
Liz-2711 <lizjrz11@unitec.edu>
2024-01-29 18:45:44 -0600

    Your commit message here

--- NewProyect/routes/index.js
+++ NewProyect/routes/index.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var router = express.Router();
-import axios from 'axios';
+//import axios from 'axios';
 
-router.get('/', async function(req, res, next) {
-  try {
-    // Make a GET request using Axios
-    const response = await axios.get('https://api.example.com/data');
+// router.get('/', async function(req, res, next) {
+//   try {
+//     // Make a GET request using Axios
+//     const response = await axios.get('https://api.example.com/data');
 
-    // Render the 'index' view with the response data
-    res.render('index', { title: 'branchhh', data: response.data });
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    // Handle the error as needed
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'branchhh' });
-
-
+//     // Render the 'index' view with the response data
+//     res.render('index', { title: 'branchhh', data: response.data });
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     // Handle the error as needed
+//     res.status(500).send('Internal Server Error');
+//   }
 // });
 
+/* GET home page. */
+ router.get('/', function(req, res, next) {
+  res.render('index', { title: 'branchhh' });
+
+ });
+
+
 module.exports = router;
+
+
+
 //view puede /* archivos estaticos, pdfs,  crear utils, config, middle ware,  */
--- NewProyect/routes/users.js
+++ NewProyect/routes/users.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var router = express.Router();
+//import dotenv from 'dotenv'
+//dotenv.config()
+var axios = require('axios');
 
  
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+ // res.send('respond with a resource');
+
+  res.send(`Hello ${process.env.NAME}`)
+  
+//console.log(process.env)
+
+
 });
+
+router.get('/Weather', async (req, res, next) => {
+
+  try {
+  const options = {
+      method: 'GET', 
+      url: `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.WEATHER_API}`
+  };
+
+  var response = await axios.request(options);
+  console.log(response.data)
+  res.send(response.data);
+} catch (e) {
+    console.error("Error al obtener acciones:", e);
+    
+    res.send(e)
+}  
+  
+});
+
 
 module.exports = router;
